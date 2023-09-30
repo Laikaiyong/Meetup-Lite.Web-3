277b5650bea2e7054b542d7fc0961dc780ea094b
Vandyck <vandycklai@gmail.com>
2023-09-30 23:19:32 +0800

    Push Gnosis builder api

--- frontend/src/pages/funds/Funds.tsx
+++ frontend/src/pages/funds/Funds.tsx
@@ -1,30 +1,36 @@
-import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonMenuButton, IonPage, IonText, IonTitle, IonToolbar } from '@ionic/react';
-import { walletOutline, walletSharp } from 'ionicons/icons';
+import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonMenuButton, IonPage, IonText, IonTitle, IonToolbar, IonList, IonItem, IonLabel } from '@ionic/react';
+import { checkmarkCircleOutline, checkmarkCircleSharp, closeCircleOutline, closeCircleSharp, walletOutline, walletSharp } from 'ionicons/icons';
 
 import { WalletAdapterNetwork, WalletNotConnectedError } from '@solana/wallet-adapter-base';
 import { ConnectionProvider, WalletProvider, useConnection, useWallet } from '@solana/wallet-adapter-react';
 import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import { Button } from '@solana/wallet-adapter-react-ui/lib/types/Button';
 
-import {
-    CloverWalletAdapter,
-    LedgerWalletAdapter,
-    PhantomWalletAdapter,
-    SolflareWalletAdapter,
-    TorusWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
-import fs from "fs";
 
+import './Funds.css';
 import { clusterApiUrl, Transaction, SystemProgram, Keypair, LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
-import React, { FC, ReactNode, useMemo, useCallback, useState } from 'react';
+import React, { FC, ReactNode, useMemo, useCallback, useState, useEffect } from 'react';
 
 import { actions, utils, programs, NodeWallet, Connection} from '@metaplex/js'; 
 import { Wallet } from '../../components/funds/ConnectWalletContext';
-
+import axios from 'axios';
 
 const Funds: React.FC = () => {
+  const transactionState  = 0;
+  // const [ transactionState, setTransactionState ] = useState(0);
   const { connection } = useConnection();
   const { publicKey, sendTransaction } = useWallet();
+
+  async function gnosisTransactionValidator(transaction: string)
+  {
+    const apiKey = "3JQMC1836DIPQQV3Q37E7U1UGC7HFG7AAJ";
+    const apiUri = "https://api.gnosisscan.io/api?module=transaction&action=gettxreceiptstatus&txhash=" + transaction + "&apikey=" + apiKey;
+
+    const apiResult = await axios.get(apiUri);
+    return apiResult.data.status as number;
+  }
+
+  const transactionCode =
+    "0x734b306be08378fb972e1a15020cb198fc4511775b84880ab15eef5fd24b2503";
 
   const onClick = useCallback(async () => {
       if (!publicKey) throw new WalletNotConnectedError();
@@ -68,6 +74,18 @@ const Funds: React.FC = () => {
       <IonContent fullscreen className="funds-content">
         <IonTitle className='funds-page-title' size="large">Fund the group</IonTitle>
         <IonText className='funds-description-text'>Fund for your favorite groups</IonText>
+
+
+        <IonList inset={true}>
+              <IonItem button onClick={gnosisTransactionValidator(transactionCode)}>
+              <IonLabel>{transactionCode}</IonLabel>
+              <IonLabel>{(!transactionState == 1)
+              ? <IonIcon aria-hidden="true" ios={checkmarkCircleOutline} md={checkmarkCircleSharp} />
+              : <IonIcon aria-hidden="true" ios={closeCircleOutline} md={closeCircleSharp} />
+            }</IonLabel>
+              <IonLabel>Click to Revalidate</IonLabel>
+            </IonItem>
+        </IonList>
       </IonContent>
     </IonPage>
     </Wallet>
